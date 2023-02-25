e124e3441ceb49870895a1736e77474fcee94fce
Alessandro <alessandro.maugeri00@gmail.com>
2023-02-25 10:28:13 +0000

    Included methods 2 and 4, began linting code.

--- Agros.py
+++ Agros.py
@@ -1,7 +1,8 @@
-from matplotlib import pyplot as plt
 import urllib.request
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class AgrosClass:
     """
@@ -49,9 +50,132 @@ class AgrosClass:
         df_agros = pd.read_csv(file_path)
         return df_agros
     
+    def countries_list(self):
+        """
+        Returns all distinct entries in the countries column of the 
+        class' dataframe in the form of a list. Removes any occurrence
+        of macro-regions or income-based geographical distributions and only
+        shows country names.
+        
+        Parameters
+        ---------------
+        self
+        Refers to the class to which the module belongs to.
+        
+        Returns
+        ---------------
+        countries: list
+        A list of all distinct countries in the dataframe.
+        """
+        countries = list(self.df_agros["Entity"].unique())
+        non_countries = ["Central Africa", "Central America",
+                "Central Asia", "Central Europe",
+                 "Developed Asia", "Developed Countries",
+                 "East Africa", "Eastern Europe", "Europe",
+                 "Former Soviet Union", "High income", 
+                 "Horn of Africa", "Latin America and the Caribbean",
+                 "Least developed countries", "Low income",
+                 "Lower-middle income", "North Africa", "North America",
+                 "Northeast Asia", "Northern Europe", "Oceania",
+                 "Pacific", "Sahel", "Serbia and Montenegro",
+                 "World", "Western Europe", "West Asia",
+                 "West Africa", "Western Europe",
+                 "Upper-middle income", "Sub-Saharan Africa",
+                 "Southern Europe", "Southern Africa", "Southern Asia"]
+        
+        cleaned_countries = [i for i in countries if i not in non_countries]
+        return cleaned_countries
+    
+    def area_chart (self, country:str, normalize:bool):
+        """
+        Plots an area chart tracking the evolution of output quantity
+        segmented by type (crop, animal and fish) over time. A
+        string can be passed to the "country" argument to select the
+        country for which data is diplayed, passing None or "World"
+        will cause the method to use globally aggregated data. When
+        the "normalize" argument is set to "True", the figures are no
+        longer displayed in absolute terms, but in percentage of the
+        total output.
+           
+        Parameters
+        ---------------
+        country: str
+        The country for which data is being plotted, passing "World"
+        or None will yield globally aggregated data.
+        
+        normalize: bool
+        A boolean that signals whether data should be presented
+        as a percentage of total output (when True) or in absolute
+        units (when False).
+            
+        Returns
+        ---------------
+        Plots the figure requested.
+        """
+        sns.set_theme()
+        output_df = self.df_agros[["Entity",
+                                "Year",
+                                "output_quantity",
+                                "crop_output_quantity",
+                                "animal_output_quantity",
+                                "fish_output_quantity"]]
+
+        if country in self.countries_list():
+            plot_df = output_df[output_df["Entity"]==country]
+            if normalize is False:
+                plt.stackplot(plot_df["Year"],
+                              plot_df["crop_output_quantity"]/10**9,
+                              plot_df["animal_output_quantity"]/10**9,
+                              plot_df["fish_output_quantity"]/10**9)
+                plt.ylabel("Output Quantity by Type (Billions)")
+            if normalize is True:
+                plt.stackplot(plot_df["Year"],
+                              (plot_df["crop_output_quantity"]/plot_df["output_quantity"])*100,
+                              (plot_df["animal_output_quantity"]/plot_df["output_quantity"])*100,
+                              (plot_df["fish_output_quantity"]/plot_df["output_quantity"])*100)
+                plt.ylabel("% of Output by Type")
+
+            plt.xlabel("Year")
+            plt.legend(["Crop", "Animal", "Fish"])
+            
+        elif country in [None, "World"]:
+            plot_df = pd.DataFrame()
+            plot_df["year_total"] = output_df[["output_quantity", "Year"]]\
+            .groupby(["Year"]).sum()/10**9
+            plot_df["crop_total"] = output_df[["crop_output_quantity", "Year"]]\
+            .groupby(["Year"]).sum()/10**9
+            plot_df["animal_total"] = output_df[["animal_output_quantity", "Year"]]\
+            .groupby(["Year"]).sum()/10**9
+            plot_df["fish_total"] = output_df[["fish_output_quantity", "Year"]]\
+            .groupby(["Year"]).sum()/10**9
+            
+            if normalize is False:
+                plt.stackplot(output_df["Year"].unique(),
+                              plot_df["crop_total"],
+                              plot_df["crop_total"],
+                              plot_df["fish_total"])
+                plt.ylabel("Output Quantity by Type (Billions)")
+                
+            if normalize is True:
+                plt.stackplot(output_df["Year"].unique(),
+                              (plot_df["crop_total"]\
+                               /plot_df["year_total"])*100,
+                              (plot_df["animal_total"]\
+                               /plot_df["year_total"])*100,
+                              (plot_df["fish_total"]\
+                               /plot_df["year_total"])*100)
+                plt.ylabel("% of Output by Type")
+                
+            plt.xlabel("Year")
+            plt.legend(["Crop", "Animal", "Fish"])
+            
+        else:
+            raise ValueError("Inserted Country is not in Dataset")
+    
     def __gapminder__(year):
         """
-        This method plots a scatter plot of fertilizer_quantity vs output_quantity with the size of each dot determined
+        This method plots a scatter plot of fertilizer_quantity vs 
+        output_quantity with the size of each dot determined
         by the Total factor productivity , for a given year.
 
         :param year: Year of the harvest. Used for the scatter plot.
@@ -67,8 +191,9 @@ class AgrosClass:
 
         # Plot the scatter plot
         fig, ax = plt.subplots()
-        ax.scatter(agriculture_filtered_df['fertilizer_quantity'], agriculture_filtered_df['output_quantity'],
-                s=agriculture_filtered_df['tfp'], alpha=0.6)
+        ax.scatter(agriculture_filtered_df['fertilizer_quantity'], 
+                   agriculture_filtered_df['output_quantity'],
+                   s=agriculture_filtered_df['tfp'], alpha=0.6)
         ax.set_xlabel('Fertilizer Quantity')
         ax.set_ylabel('Output Quantity')
         ax.set_title(f'Crops Production in {year}')
